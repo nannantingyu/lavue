0bdc5ce149fcb44ec257615943de4d739107d1c2
yangmingming <yangmingming@91guoxin.com>
2017-09-13 14:26:10 +0800

    remove H:I:S

--- resources/views/index/search.blade.php
+++ resources/views/index/search.blade.php
@@ -6,7 +6,7 @@
                 @foreach($articles as $vo)
                     <li>
                         <div class="title"><a href="{{ $base_url }}blog_{{ $vo->id }}">{{ $vo->title }}</a></div>
-                        <div class="time">{{  date("Y-m-d H:i:s", strtotime($vo->publish_time)) }}</div>
+                        <div class="time">{{  date("Y-m-d", strtotime($vo->publish_time)) }}</div>
                         <div class="clear"></div>
                     </li>
                 @endforeach
