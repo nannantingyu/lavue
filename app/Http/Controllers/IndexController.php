76e3f7720f5f08108d4a441f67ca6ac44aafe004
yangmingming <yangmingming@91guoxin.com>
2017-12-13 09:38:55 +0800

    sort time

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -184,6 +184,10 @@ class IndexController extends Controller
 
         $all_keys = array_merge($hotkey, $hotkey2);
 
+        usort($all_keys, function($a, $b){
+            return $a['time'] > $b['time'];
+        });
+
         return $all_keys;
     }
 }
