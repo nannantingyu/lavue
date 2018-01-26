ae146e4553cc6476f84d0135ab35b84cee2adf12
yangmingming <yangmingming@91guoxin.com>
2018-01-26 11:14:45 +0800

    添加导航链接

--- resources/views/public/friendlink.blade.php
+++ resources/views/public/friendlink.blade.php
@@ -5,6 +5,7 @@
             <div class="cnt">
                 <a href="http://cj.yjshare.cn/" target="_blank">财经分享</a><span>|</span>
                 <a href="http://ads.yjshare.cn/" target="_blank">链家工具</a><span>|</span>
+                <a href="https://www.yjshare.cn/hq_btc" target="_blank">区块链行情</a><span>|</span>
             </div>
         </li>
     </ul>
