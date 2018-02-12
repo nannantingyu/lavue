987417ca60dad60f8b31951e80cd03bee1f856fb
yangmingming <yangmingming@91guoxin.com>
2018-02-12 10:05:02 +0800

    图片宽度100%

--- public/js/common.js
+++ public/js/common.js
@@ -1,3 +1,11 @@
 $(function(){
     $(".content-left img").parents('p').css("text-align", "center");
+    $(".content-left img").each(function(index, img){
+        img.onload = function() {
+            if($(img).width() > $(".content-left").width()) {
+                $(img).removeAttr("style").css("width", "100%");
+                img.style.width = "100% !important";
+            }
+        };
+    });
 });
