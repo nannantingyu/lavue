f40e8912964c8a45d1a62ccfc7ae3b4b1b336fc6
杨明明 <yangmingming@jujin8.com>
2018-09-21 12:02:59 +0800

    排序

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -391,6 +391,7 @@ class AccountController extends Controller
                     ->where('wx_id', $wx_id)
                     ->where('start_time', '>=', $start_date)
                     ->where('end_time', '<=', $end_date)
+                    ->orderBy('start_time', 'desc')
                     ->get();
             }
             elseif($type === 'account_type') {
@@ -399,12 +400,14 @@ class AccountController extends Controller
                     ->where('wx_id', $wx_id)
                     ->where('start_time', '>=', $start_date)
                     ->where('end_time', '<=', $end_date)
+                    ->orderBy('start_time', 'desc')
                     ->get();
             }
             elseif($type === 'type') {
                 $data = WxAccountLog::where('wx_id', $wx_id)
                     ->where('start_time', '>=', $start_date)
                     ->where('end_time', '<=', $end_date)
+                    ->orderBy('start_time', 'desc')
                     ->get();
             }
 
