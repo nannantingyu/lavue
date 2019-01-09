ed9a752d27d94ffd847d058e3f36d765bf5ab06a
杨明明 <yangmingming@jujin8.com>
2019-01-09 11:14:40 +0800

    修改字段名称

--- app/Models/ArticleBody.php
+++ app/Models/ArticleBody.php
@@ -4,5 +4,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class ArticleBody extends Model {
     protected $table = 'weixin_article_detail';
+    const UPDATED_AT = 'updated_time';
+    const CREATED_AT = 'created_time';
     protected $fillable = ['id', 'body'];
 }
